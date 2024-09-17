ed975aead4a199fe8880d26a84f6c8e257095329
shubh dadhich <69069614+shubhdadhich01@users.noreply.github.com>
2024-09-17 19:53:35 +0530

    Update Kubeadm_master.sh

--- Cluster_Setup/Kubeadm_master.sh
+++ Cluster_Setup/Kubeadm_master.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
+### Disable swap
+```
 swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+```
+### Configure System Networking
 
+```
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
-
+```
+```
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
-# sysctl params required by setup, params persist across reboots
+```
+```
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
+```
 
-# Apply sysctl params without reboot
+### Apply sysctl params without reboot
+
+```
 sudo sysctl --system
+```
 
+### Adding Kubernetes repository
+
+```
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 
@@ -28,11 +41,19 @@ sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+```
 
+### Installing Kubeadm 
+
+```
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+```
 
+### Adding Docker Repository
+
+```
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -42,24 +63,43 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
+```
+### Installing Docker and Containerd
+
+```
 sudo apt-get update
 sudo apt-get install containerd.io docker-ce docker-ce-cli docker-buildx-plugin docker-compose-plugin -y
+```
+### Configuring Systemd Cgroup for contanerd
 
+```
 mkdir -p /etc/containerd
 containerd config default | tee /etc/containerd/config.toml
 sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+```
+### Restarting Containerd
+
+```
 sudo systemctl restart containerd
 sudo systemctl enable containerd
-
 sudo systemctl status containerd
-  
-kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr=192.168.0.0/16
+```  
+### Initializing Kubeadm
 
+```
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr=192.168.0.0/16
+```
+### Configure kubectl for the User
+
+```
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
+```
+```
 export KUBECONFIG=/etc/kubernetes/admin.conf
-
+```
+### Installing Calico CNI
+```
 sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/calico.yaml
+```
